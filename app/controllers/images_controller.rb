848d910d584551358f14b3de3fa315fe6abd6344
Jon Evans <jle@foraker.com>
2015-03-15 23:26:53 -0600

    Resize image based on width and height params

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,6 +3,9 @@ class ImagesController < ApplicationController
     image = Image.all.sample
     image = Magick::Image::read(image.file.path).first
     image.format = 'jpg'
+    if params[:width] && params[:height]
+      image = image.resize_to_fill(params[:width].to_i, params[:height].to_i)
+    end
     send_data image.to_blob, disposition: :inline, type: 'image/jpg'
   end
 end
